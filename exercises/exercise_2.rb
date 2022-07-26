465df62b1a64b1cf3e832bc274b211db5867c584
Dylan Wang <wyc6990906@hotmail.com>
2022-07-25 19:30:55 -0600

    finish exercise_2.rb

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+#Assign store1 & store2 to instance vars
+@store1 = Store.find_by(id: 1);
+@store2 = Store.find_by(id: 2);
+@store3 = Store.find_by(id: 3);
+
+#Update store's name
+@store1.name = 'Montreal'
